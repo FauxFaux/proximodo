6df66ef1a7cc89e729f9162459e7fd93b9c05790
ryo-s <ryo-s>
2005-05-16 16:29:04 +0000

    Set %NSIS_HOME% if missing.

--- installer/makeinstaller.bat
+++ installer/makeinstaller.bat
@@ -3,4 +3,5 @@ REM This script creates the installer for Proximodo on Windows.
 REM It has only been tested on Windows XP but should work on most Windows flavours.
 REM You have to set the NSIS_HOME environment variable before running the script.
 
+IF "%NSIS_HOME%" == "" SET NSIS_HOME=%ProgramFiles%\NSIS
 "%NSIS_HOME%\makensis.exe" proximodo.nsi
